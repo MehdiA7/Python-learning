781f58d97e8b865fff86196e4996e6c7971c99d7
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2024-06-19 17:54:37 +0200

    Update Most_Used_Word_Genius_ARTIST.py

---
+++ Challenge/Duplicate_File_Detector.py
@@ -0,0 +1,40 @@
+import hashlib
+from pathlib import Path
+from pprint import pprint
+
+load = 1
+path = Path.home() / "Downloads"
+FILE_HASHCODE = {}
+
+
+def hash_code(path):
+    hash_data = hashlib.sha256()
+    with open(path, 'rb') as f:
+        for octet in iter(lambda: f.read(4096), b""):
+            hash_data.update(octet)
+    FILE_HASHCODE[path.name] = hash_data.hexdigest()
+    return FILE_HASHCODE
+
+
+def get_all_file():
+    # Touver pourquoi le résultat retourne WindowsPath et pas juste le chemin
+    return [f for f in path.rglob('*') if f.is_file()]
+    # return list(path.rglob('*'))
+
+
+all_path = get_all_file()
+for way in all_path:
+    print(f"File Analysis : {load}")
+    load += 1
+    hash_code(way)
+
+# Create a dictionary return to detect duplicates files
+DUPLICATE_FILE = {}
+for file_name, code in FILE_HASHCODE.items():
+    DUPLICATE_FILE.setdefault(code, set()).add(file_name)
+
+result = [key for key, values in DUPLICATE_FILE.items() if len(values) > 1]
+print("Here are the files that are duplicates in your folders : ")
+for duplicate_file in result:
+    print(DUPLICATE_FILE[duplicate_file])
+
--- Script _API/Most_Used_Word_Genius_ARTIST.py
+++ Script _API/Most_Used_Word_Genius_ARTIST.py
@@ -6,44 +6,66 @@ from pprint import pprint
 
 
 def get_all_urls() -> list:
+    # Declare the variables
     next_page = ''
     page = 1
     links = []
-    artist_code = "14693"
-    while next_page != None:
+    artist_code = "385596"
+    while next_page is not None:
+        # -API-
         api_link = requests.get(f"https://genius.com/api/artists/{artist_code}/songs?page={page}&sort=popularity")
+        # Check the answer
         if api_link.status_code == 200:
+            # Get keys in json file
             response = api_link.json().get("response")
             next_page = response.get("next_page")
             songs = response.get("songs")
+            # Configure the loader
             print(f"Page Number : {page}")
             page += 1
+            # Add all link in a list
             links.extend([song.get('url') for song in songs])
+        # Configure with code for HTTP response
         else:
             print(f"Page -> We have an error : {api_link.status_code}")
+
     return links
 
 
 def extract_word(urls):
+    # Declare the variables
     all_words = []
     load = 1
+    # Extract link 1 by 1
     for url in urls:
         link = requests.get(url)
+        # Check the answer
         if link.status_code == 200:
+            # Scrap the HTML code
             soup = BeautifulSoup(link.content, 'html.parser')
+            # Take lyrics with class name
             lyrics = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-1 kUgSbL")
+            # Configure de loader
             print(f"Song analysis : {load}")
             load += 1
+            # Extract all word and sort
             try:
+                # Clean HTML
                 for sentence in lyrics.stripped_strings:
+                    # Extract word by word
                     for word in sentence.split(" "):
+                        # Add condition for sort words
                         if len(word) > 4 and '[' not in word and ']' not in word:
                             sort_word = word.strip(':').strip('.').strip(',').lower().split("\n")
+                            # Input all sort word in a single list
                             [all_words.append(word) for word in sort_word]
+            # Configure an error for no lyrics
             except AttributeError:
-                print("Not a song")
+                print("No Lyrics")
+        # Configure an error with code for HTTP response
         else:
             print(f"Song Link -> We have an error : {link.status_code}")
+    # Count and class most used word in lyrics
     counter = Counter(all_words)
     pprint(counter.most_common(100))
 
